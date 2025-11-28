52378c6b9a36cf2624e9130e08b3fd9ab990ac9b
aaron Ries <hobbydorkowner@gmail.com>
2025-11-28 22:54:52 +0000

    rompt to give Firestudio
    The sidebar/navigation drawer is breaking the

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,26 +1,20 @@
-import { SidebarProvider, Sidebar, SidebarInset } from '@/components/ui/sidebar';
+import { SidebarProvider, Sidebar } from '@/components/ui/sidebar';
 import Header from './Header';
 import SidebarNav from './SidebarNav';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
-  // We can read the cookie for the default open state
-  // const layout = cookies().get("react-resizable-panels:layout")
-  // const defaultLayout = layout ? JSON.parse(layout.value) : undefined
-
   return (
     <SidebarProvider>
       <div className="relative flex min-h-svh flex-col bg-background">
         <Sidebar>
           <SidebarNav />
         </Sidebar>
-        <SidebarInset>
-          <div className="flex flex-col h-screen">
-            <Header />
-            <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8">
-              {children}
-            </main>
-          </div>
-        </SidebarInset>
+        <div className="flex flex-col h-screen">
+          <Header />
+          <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8">
+            {children}
+          </main>
+        </div>
       </div>
     </SidebarProvider>
   );
