2956d8f3c003ce9c8493d6a485638c228f0466af
aaron Ries <hobbydorkowner@gmail.com>
2025-11-28 22:55:42 +0000

    now adjust the main pages so the sidebar isnt covering

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,20 +1,35 @@
-import { SidebarProvider, Sidebar } from '@/components/ui/sidebar';
+
+'use client';
+import { SidebarProvider, Sidebar, useSidebar } from '@/components/ui/sidebar';
 import Header from './Header';
 import SidebarNav from './SidebarNav';
+import { cn } from '@/lib/utils';
+
+function MainContent({ children }: { children: React.ReactNode }) {
+  const { open, isMobile } = useSidebar();
+  return (
+    <div
+      className={cn(
+        'flex flex-col h-screen transition-all duration-300 ease-in-out',
+        { 'md:ml-64': open && !isMobile }
+      )}
+    >
+      <Header />
+      <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8">
+        {children}
+      </main>
+    </div>
+  );
+}
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
-      <div className="relative flex min-h-svh flex-col bg-background">
+      <div className="relative min-h-svh bg-background">
         <Sidebar>
           <SidebarNav />
         </Sidebar>
-        <div className="flex flex-col h-screen">
-          <Header />
-          <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8">
-            {children}
-          </main>
-        </div>
+        <MainContent>{children}</MainContent>
       </div>
     </SidebarProvider>
   );
