69822be6b7606f0219873d9b1e76fd1af789e476
aaron Ries <hobbydorkowner@gmail.com>
2025-12-01 15:14:06 +0000

    Find and fix Firebase project configuration
    Search the entire project f

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-4668517724-751eb",
+  "projectId": "PUT_MY_PROJECT_ID_HERE",
   "appId": "1:954197584480:web:02ace0d70d05a8ed32f105",
   "apiKey": "AIzaSyBvW-C8vsY8e_GpJG1Ly05x3CinPhmqo-w",
-  "authDomain": "studio-4668517724-751eb.firebaseapp.com",
+  "authDomain": "PUT_MY_PROJECT_ID_HERE.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "954197584480"
 };
