3a8659c98ad2b86f97c8492159e510b2d65945d0
aaron Ries <hobbydorkowner@gmail.com>
2025-12-02 06:57:58 +0000

    Please do better man

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -23,21 +23,21 @@ const LegoHead = ({ className }: { className?: string }) => (
             <path d="M35 55 Q50 65 65 55" stroke="hsl(var(--background))" strokeWidth="3" fill="none" />
 
             {/* Crossbones */}
-            <g transform="translate(0, 55)">
-              <g transform="rotate(-30 50 25)">
-                <rect x="20" y="22.5" width="60" height="5" rx="2.5" />
-                <circle cx="20" cy="20" r="7" />
-                <circle cx="20" cy="30" r="7" />
-                <circle cx="80" cy="20" r="7" />
-                <circle cx="80"cy="30" r="7" />
-              </g>
-              <g transform="rotate(30 50 25)">
-                <rect x="20" y="22.5" width="60" height="5" rx="2.5" />
-                <circle cx="20" cy="20" r="7" />
-                <circle cx="20" cy="30" r="7" />
-                <circle cx="80" cy="20" r="7" />
-                <circle cx="80" cy="30" r="7" />
-              </g>
+            <g transform="translate(0, 65) scale(0.9)">
+                <g transform="rotate(-30 50 25)">
+                    <path d="M 15 25 C 15 15, 25 15, 25 25 S 15 35, 15 25 Z" />
+                    <path d="M 25 25 C 25 15, 35 15, 35 25 S 25 35, 25 35 Z" transform="translate(40, 0) scale(-1, 1) translate(-60, 0)" />
+                    <rect x="22" y="22.5" width="56" height="5" rx="2.5" />
+                    <path d="M 75 25 C 75 15, 85 15, 85 25 S 75 35, 75 25 Z" />
+                    <path d="M 85 25 C 85 15, 95 15, 95 25 S 85 35, 85 35 Z" transform="translate(140, 0) scale(-1, 1) translate(-180, 0)" />
+                </g>
+                <g transform="rotate(30 50 25)">
+                    <path d="M 15 25 C 15 15, 25 15, 25 25 S 15 35, 15 25 Z" />
+                    <path d="M 25 25 C 25 15, 35 15, 35 25 S 25 35, 25 35 Z" transform="translate(40, 0) scale(-1, 1) translate(-60, 0)" />
+                    <rect x="22" y="22.5" width="56" height="5" rx="2.5" />
+                    <path d="M 75 25 C 75 15, 85 15, 85 25 S 75 35, 75 25 Z" />
+                    <path d="M 85 25 C 85 15, 95 15, 95 25 S 85 35, 85 35 Z" transform="translate(140, 0) scale(-1, 1) translate(-180, 0)" />
+                </g>
             </g>
         </g>
     </svg>
