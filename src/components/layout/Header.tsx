e402dfc13487209f4e6031b761d6fa724ba8aa51
aaron Ries <hobbydorkowner@gmail.com>
2025-12-02 22:08:10 +0000

    Not whiter wider

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -14,7 +14,7 @@ export default function Header() {
   
   return (
     <>
-      <header className="sticky top-0 z-30 flex h-16 items-center gap-4 border-b bg-card px-4 md:px-8 lg:px-12">
+      <header className="sticky top-0 z-30 flex h-16 items-center gap-4 border-b bg-card px-4 md:px-10 lg:px-16">
         
         {/* This button is only for toggling the sidebar on mobile and should be hidden on desktop */}
         <SidebarTrigger className="md:hidden" />
